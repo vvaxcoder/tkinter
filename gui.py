eaa3314a43284eda18d94b9c1ee93cd4de55f149
Valerii Voronkov <scorpirat@gmail.com>
2021-06-20 17:46:08 +0300

    Use grid.

--- gui.py
+++ gui.py
@@ -3,8 +3,15 @@ from tkinter import *
 root = Tk()
 
 # Create a Label widget
-myLabel = Label(root, text="Hello World")
+myLabel1 = Label(root, text="Label 1")
+myLabel2 = Label(root, text="Label2")
+
 # Showing it onto the screen
-myLabel.pack()
+# myLabel.pack()
 # run event loop
+
+#use grid system
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
 root.mainloop()
