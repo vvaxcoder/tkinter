61dc46e18bce3b710effe577af08f7d48a08a34c
Valerii Voronkov <scorpirat@gmail.com>
2021-06-20 13:41:10 +0300

    Create first GUI application.

---
+++ gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+# Create a Label widget
+myLabel = Label(root, text="Hello World")
+# Showing it onto the screen
+myLabel.pack()
+# run event loop
+root.mainloop()
