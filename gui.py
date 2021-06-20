3b635b838af2c6f0635bfd581f045bba45a0b4be
Valerii Voronkov <scorpirat@gmail.com>
2021-06-20 18:11:35 +0300

    Another example of using grid.

--- gui.py
+++ gui.py
@@ -2,16 +2,12 @@ from tkinter import *
 
 root = Tk()
 
-# Create a Label widget
-myLabel1 = Label(root, text="Label 1")
-myLabel2 = Label(root, text="Label2")
+# Create a Label widget and use grid system
+myLabel1 = Label(root, text="Label 1").grid(row=0, column=0)
+myLabel2 = Label(root, text="Label2").grid(row=1, column=0)
 
 # Showing it onto the screen
 # myLabel.pack()
 # run event loop
-
-#use grid system
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
 
 root.mainloop()
