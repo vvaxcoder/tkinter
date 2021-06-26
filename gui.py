7664d21aea6133c5808242f12f93180842fc4569
Valerii Voronkov <scorpirat@gmail.com>
2021-06-26 11:25:03 +0300

    Add function and call it after the click on the button.

--- gui.py
+++ gui.py
@@ -2,8 +2,12 @@ from tkinter import *
 
 root = Tk()
 
+def myClick():
+    myLabel = Label(root, text="The button has been clicked!")
+    myLabel.pack()
+
 # myButton = Button(root, text="Click me!", state=DISABLED)
-myButton = Button(root, text="Click me!", padx=50)
+myButton = Button(root, text="Click me!", padx=50, command=myClick)
 myButton.pack()
 
 # run event loop
