5d00eb99722ac81e0fd93583a140570d3f062e99
Valerii Voronkov <scorpirat@gmail.com>
2021-06-26 09:29:57 +0300

    Add buttons.

--- gui.py
+++ gui.py
@@ -2,12 +2,9 @@ from tkinter import *
 
 root = Tk()
 
-# Create a Label widget and use grid system
-myLabel1 = Label(root, text="Label 1").grid(row=0, column=0)
-myLabel2 = Label(root, text="Label2").grid(row=1, column=0)
+# myButton = Button(root, text="Click me!", state=DISABLED)
+myButton = Button(root, text="Click me!", padx=50)
+myButton.pack()
 
-# Showing it onto the screen
-# myLabel.pack()
 # run event loop
-
 root.mainloop()
